df08dc8df5c321de8200c1ad10e6a3142e0ccf1a
João Victor Diniz <133976920+joaoDinizAires@users.noreply.github.com>
2023-08-25 16:35:26 -0300

    Delete processar.php

--- processar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once ("API/viaCEP.php");
-    if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['cep'])){
-        $cep = $_POST['cep'];
-        $cepData = ViaCep::consultarCep($cep);
-        return $cepData;
-    }
-?>
